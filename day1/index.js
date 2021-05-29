771557fc38e98d5ac99628bb42e24d542a84bdf1
unknown <48155874+generic-placeholder-name@users.noreply.github.com>
2021-05-29 21:48:57 +0700

    HW lesson 1

--- day1/index.js
+++ day1/index.js
@@ -1,1 +1,41 @@
-alert('lololol')
+//day 1 homework
+
+//task 3
+let message = 'Coding is great'
+console.log(message)
+let studentCount = 0
+console.log(studentCount)
+
+//task 4
+message = 'Coding might not be easy, but still great'
+console.log(message)
+studentCount = 16
+console.log(studentCount)
+message.toLowerCase()
+console.log(message)
+studentCount--
+console.log(studentCount)
+
+//task 5
+alert('this is task 5')
+
+//task 6
+let personName = prompt('Input name: ')
+alert(`Hi ${personName}`)
+
+//task 7
+let firstName = prompt('Input first name: ')
+let lastName = prompt('Input last name: ')
+alert(`Hi ${firstName} ${lastName}`) //assuming Western name order
+
+//task 8
+let a = Number(prompt('Input square side: '))
+alert(`Square area = ${a * a}`)
+
+//task 9
+let r = Number(prompt('Input circle radius: '))
+alert(`Square area = ${r * r * Math.PI}`)
+
+//task 10
+let degC = Number(prompt('Input deg C: '))
+alert(`${degC} (C) = ${degC * 9 / 5 + 32} (F)`)
